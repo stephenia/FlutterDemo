d4f7eb3fa47e16a149bda9af6c814014a1b3610a
huangqi <844306539@qq.com>
2020-09-21 10:20:59 +0800

    Initial Commit

---
+++ Flutter_Add_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flutter_Add_iOS
+//
+//  Created by 柒哥 on 2020/9/21.
+//  Copyright © 2020 柒哥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flutter_Add_iOSTests/Flutter_Add_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Flutter_Add_iOSTests.swift
+//  Flutter_Add_iOSTests
+//
+//  Created by 柒哥 on 2020/9/21.
+//  Copyright © 2020 柒哥. All rights reserved.
+//
+
+import XCTest
+@testable import Flutter_Add_iOS
+
+class Flutter_Add_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
